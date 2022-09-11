ab17c276024df6ba6446a607ba908273202ef0ce
TobitaKosuke <ktobita7w9@gmail.com>
2022-09-11 22:40:45 +0900

    選択リスト

--- hello/forms.py
+++ hello/forms.py
@@ -5,6 +5,8 @@ class HelloForm(forms.Form):
     data=[
         ('one','item 1'),
         ('two','item 2'),
-        ('three','item 3')
+        ('three','item 3'),
+        ('four','item 4'),
+        ('five','item 5')
         ]
-    choice=forms.ChoiceField(label="radio" ,choices=data ,widget=forms.RadioSelect())
+    choice=forms.ChoiceField(label="radio" ,choices=data ,widget=forms.Select(attrs={"size": 5}))
