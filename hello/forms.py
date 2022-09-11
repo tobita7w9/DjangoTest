efde79c58052d1f03c59140588a285bf30ee39e1
TobitaKosuke <ktobita7w9@gmail.com>
2022-09-11 22:46:08 +0900

    複数項目選択

--- hello/forms.py
+++ hello/forms.py
@@ -9,4 +9,4 @@ class HelloForm(forms.Form):
         ('four','item 4'),
         ('five','item 5')
         ]
-    choice=forms.ChoiceField(label="radio" ,choices=data ,widget=forms.Select(attrs={"size": 5}))
+    choice=forms.ChoiceField(label="radio" ,choices=data ,widget=forms.SelectMultiple(attrs={"size": 6}))
--- hello/views.py
+++ hello/views.py
@@ -17,7 +17,7 @@ class HelloView(TemplateView):
         return render(request, "hello/index.html",self.params)
 
     def post(self, request):
-        ch=request.POST["choice"]
-        self.params["result"]="you selected: '"+ch+"'."
+        ch=request.POST.getlist("choice")
+        self.params["result"]="you selected: '"+str(ch)+"'."
         self.params["form"]=HelloForm(request.POST)
         return render(request,"hello/index.html" ,self.params)
