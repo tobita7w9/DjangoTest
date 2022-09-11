366efe26507b3d84727aa884a836b1bfd078aad6
TobitaKosuke <ktobita7w9@gmail.com>
2022-09-11 22:13:41 +0900

    チェックボックスの実装

--- hello/forms.py
+++ hello/forms.py
@@ -2,6 +2,4 @@
 from django import forms
 
 class HelloForm(forms.Form):
-    name=forms.CharField(label='name')
-    mail=forms.CharField(label='mail')
-    age=forms.IntegerField(label='age')
+    check=forms.BooleanField(label="Checkbox" ,required=False)
--- hello/views.py
+++ hello/views.py
@@ -9,18 +9,18 @@ class HelloView(TemplateView):
     def __init__(self):
         self.params={
             "title":"Hello",
-            "message":"your data: ",
-            "form":HelloForm()
+            "form":HelloForm(),
+            "result":None
             }
         
     def get(self,request):
         return render(request, "hello/index.html",self.params)
 
     def post(self, request):
-        msg="あなたは<b>"+request.POST['name']+ \
-            "("+request.POST['age']+ \
-                ")</b>さんです。<br>メールアドレスは<b>"+request.POST['mail'] + \
-                "</b>ですね。"
-        self.params["message"]=msg
+        if("check" in request.POST):
+            self.params["result"]="Checked"
+        else:
+            self.params["result"]="Not Checked..."
+            
         self.params["form"]=HelloForm(request.POST)
         return render(request,"hello/index.html" ,self.params)
