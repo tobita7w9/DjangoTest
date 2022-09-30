d7d64b1068d37df7d696dc3a687e14690888c038
TobitaKosuke <ktobita7w9@gmail.com>
2022-09-30 15:21:04 +0900

    C3-3 クエリセットの表示カスタマイズ

--- hello/views.py
+++ hello/views.py
@@ -4,9 +4,22 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .forms import HelloForm
 from .models import Friend
+from django.db.models import QuerySet
 # Create your views here.
+def __new_str__(self):
+    result=''
+    for item in self:
+        result='<tr>'
+        for k in item:
+            result+='<td>'+str(k)+'='+str(item[k])+'</td>'
+        result+='</tr>'
+    return result
+
+QuerySet.__str__=__new_str__
+
+
 def index(request):
-    data=Friend.objects.all().values_list('id','name','age')
+    data=Friend.objects.all().values('id','name','age')
     params={
         'title':'Hello',
         'data':data,
